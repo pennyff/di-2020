180cada62e650c31237029f62aee6473134584e8
pennyff <60908061+pennyff@users.noreply.github.com>
2020-02-18 02:13:06 -0500

    flower

--- 2.mapping-time/hw this week/sketch.js
+++ 2.mapping-time/hw this week/sketch.js
@@ -1,13 +1,16 @@
 
 function setup() {
   // set the width & height of the sketch
-  createCanvas(400, 130)
+  createCanvas(720, 400)
 
   // print the time to the console once at the beginning of the run. try opening up the
   // web inspector and poking around to see the various values the clock function gives you
   print('starting time:', clock())
 
+// A design for a simple flower
+  translate(580, 200);
+  noStroke();
+  for (let i = 0; i < 12; i ++) {ellipse(0, 30, 20, 80);
+    rotate(PI/6);}
 }
-function draw() {
-  ellipse(80,50,100,100);
-}
+    
